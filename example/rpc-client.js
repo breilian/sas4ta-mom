34707c3a651c6416013159c806c6d2cd0f408d31
Aliaksei Breilian <aliaksei_breilian@epam.com>
2021-10-15 17:12:49 +0300

    clear code

--- example/rpc-client.js
+++ example/rpc-client.js
@@ -1,13 +1,8 @@
 let amqp = require('amqplib/callback_api');
 
-// let args = process.argv.slice(2);
+let args = process.argv.slice(2)[0];
 
-// if (args.length == 0) {
-//   console.log("Usage: rpc_client.js num");
-//   process.exit(1);
-// }
-
-amqp.connect('amqps://zqjdagji:FzLnkKffmRAZHSwC7Ec1HlM1cojTupVw@cow.rmq2.cloudamqp.com/zqjdagji', function(error0, connection) {
+amqp.connect(url, function(error0, connection) {
   if (error0) {
     throw error0;
   }
@@ -24,26 +19,14 @@ amqp.connect('amqps://zqjdagji:FzLnkKffmRAZHSwC7Ec1HlM1cojTupVw@cow.rmq2.cloudam
         throw error2;
       }
       let correlationId = generateUuid();
-    //   let input = args[0];
-
-    //   console.log(' [x] Requesting to process %s', input);
 
       channel.consume(q.queue, function(msg) {
         if (msg.properties.correlationId == correlationId) {
           console.log(' [.] Got %s', msg.content.toString());
-        //   setTimeout(function() {
-        //     connection.close();
-        //     process.exit(0)
-        //   }, 500);
         }
       }, {
         noAck: true
       });
-
-    //   channel.sendToQueue('rpc_queue',
-    //     Buffer.from(input.toString()),{
-    //       correlationId: correlationId,
-    //       replyTo: q.queue });
 
         function streamMessages() {
             let msg = Math.random().toString(36).substring(2, 3 + Math.floor(Math.random() * 4));
